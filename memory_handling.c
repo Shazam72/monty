dab408439c8ccd7ecd035f6aff5022f0e0c127d3
leviathan72 <jfrancoiszouba72@gmail.com>
2023-06-23 00:52:33 +0200

    Memory handling

--- memory_handling.c
+++ memory_handling.c
@@ -7,44 +7,38 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p = NULL;
+	void *ptr = NULL;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
-	p = malloc(nmemb * size);
-	if (p == NULL)
-	{
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
 		return (NULL);
-	}
 	for (i = 0; i < (nmemb * size); i++)
-	{
-		*((char *)(p) + i) = 0;
-	}
-	return (p);
+		*((char *)(ptr) + i) = 0;
+	return (ptr);
 }
 /**
- * _realloc - change the size and copy the content
- * @ptr: malloc pointer to reallocate
- * @old_size: old number of bytes
- * @new_size: new number of Bytes
- * Return: nothing
+ * _realloc - reallocate memory and copy the content
+ * @ptr: ptr to memory to reallocate
+ * @old_size: old size
+ * @new_size: new size
+ * Return: ptr to new reallocated memory
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p = NULL;
+	char *new_ptr = NULL;
 	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
 	{
-		p = malloc(new_size);
-		if (!p)
+		new_ptr = malloc(new_size);
+		if (!new_ptr)
 			return (NULL);
-		return (p);
+		return (new_ptr);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -53,21 +47,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size > old_size)
 	{
-		p = malloc(new_size);
-		if (!p)
+		new_ptr = malloc(new_size);
+		if (!new_ptr)
 			return (NULL);
 		for (i = 0; i < old_size; i++)
-			p[i] = *((char *)ptr + i);
+			new_ptr[i] = *((char *)ptr + i);
 		free(ptr);
 	}
 	else
 	{
-		p = malloc(new_size);
-		if (!p)
+		new_ptr = malloc(new_size);
+		if (!new_ptr)
 			return (NULL);
 		for (i = 0; i < new_size; i++)
-			p[i] = *((char *)ptr + i);
+			new_ptr[i] = *((char *)ptr + i);
 		free(ptr);
 	}
-	return (p);
+	return (new_ptr);
 }
